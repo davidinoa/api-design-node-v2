132a3727a7fc73d4d5fde4cb9cefd4883f636858
David Inoa <davidinoa@gmail.com>
2018-05-13 11:44:33 -0700

    Complete lesson #1

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
-import express from 'express'
-import setupMiddware from './middleware'
-import { restRouter } from './api'
-import { connect } from './db'
-import { signin, protect } from './api/modules/auth'
+import express from 'express';
+import setupMiddware from './middleware';
+import { restRouter } from './api';
+import { connect } from './db';
+import { signin, protect } from './api/modules/auth';
 // Declare an app from express
-const app = null
+const app = express();
 
-export default app
+// catch all
+app.all('*', (req, res) => {
+	res.json({ ok: false });
+});
+
+export default app;
